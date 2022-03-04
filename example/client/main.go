9e0792d2efdcefe95d67c342fdef9309ff8d8feb
zhangchanglin <>
2022-03-04 16:46:49 +0800

    增加测试

--- example/client/main.go
+++ example/client/main.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	c := chclient.Config{
 		Server:           "localhost:8888",
-		Remotes:          []string{"3000"},
-		Auth:             "foo:bar",
+		Remotes:          []string{"R:0.0.0.0:45320:8081"},
+		Auth:             "9af92df4-e427-4086-9841-08da393c0f5c:b5fbcf537ed1a0d284fb6c1e236de0a4",
+		KeepAlive:        30 * time.Second,
 		MaxRetryInterval: time.Minute,
 		MaxRetryCount:    -1,
 	}
@@ -19,7 +20,13 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	if err = client.Start(context.Background()); err != nil {
+	client.Debug = true
+	time.AfterFunc(10*time.Second, func() {
+		client.Close()
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	if err = client.Start(ctx); err != nil {
 		log.Fatalln("client.Start err:", err)
 	}
 	if err = client.Wait(); err != nil {
